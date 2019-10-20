0a52a9109b159d0e674f7c40b9119f0926b5fa25
Mustafa Hoda <mustafahoda@Mustafas-MacBook-Pro.local>
2019-10-20 11:20:55 -0700

    As user cant' send request

--- thread_try.py
+++ thread_try.py
@@ -7,12 +7,28 @@ box_client = app_client.client
 
 as_users = app_client.as_users
 
-set_trace()
 
 def thread_create_user(client, creator, name, login, group):
 
+    Admin3 = box_client.user(user_id='10368548905')
+
+    Admin3_client = box_client.as_user(Admin3)
+    set_trace()
+
+    new_user = Admin3_client.create_user('Macbook Pro')
+    set_trace()
+
+
+
+
+
+
 
     response = app_client.create_user(name, login, group)
+
+
+if __name__ == "__main__":
+    thread_create_user(None, None, None, None, None)
 
 
 # def thread_runner(client, tasks):
