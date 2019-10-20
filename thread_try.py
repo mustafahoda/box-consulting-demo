40c72ae15ea949445e2102e97e883aedbe5db203
Mustafa Hoda <mustafahoda@Mustafas-MacBook-Pro.local>
2019-10-20 11:39:35 -0700

    threading working

--- thread_try.py
+++ thread_try.py
@@ -1,6 +1,8 @@
 from src.Client import BoxClient
 from pdb import set_trace
 from src.users import get_user_by_email
+from concurrent.futures import ThreadPoolExecutor
+from timeit import default_timer as timer
 
 app_client = BoxClient()
 box_client = app_client.client
@@ -8,27 +10,44 @@ box_client = app_client.client
 as_users = app_client.as_users
 
 
-def thread_create_user(client, creator, name, login, group):
-
-    Admin3 = box_client.user(user_id='10368548905')
-
-    Admin3_client = box_client.as_user(Admin3)
-    set_trace()
-
-    new_user = Admin3_client.create_user('Macbook Pro')
-    set_trace()
-
-
-
-
-
+def create_user(config_tuple):
+    name = config_tuple[0]
+    login = config_tuple[1]
+    group = config_tuple[2]
 
 
     response = app_client.create_user(name, login, group)
 
 
 if __name__ == "__main__":
-    thread_create_user(None, None, None, None, None)
+
+    users_to_add = [
+        ("Jeff", "jeffmacbookpro@toodles.com", "students"),
+        ("Honest", "honestmacbookkkppproo@toodles.com", "students"),
+        ("Eyelashes", "eyelashes@toodles.com", "students"),
+        ("Lightbulb", "Ilightuptheworld@toodles.com", "students"),
+        ("eyePhone", "eyephoneschangedtheworld@toodles.com", "students"),
+        ("backpack", "backpack@toodles.com", "students")
+    ]
+
+    # Threading example
+    # start = timer()
+    # with ThreadPoolExecutor(max_workers=4) as executor:
+    #     for _ in executor.map(create_user, users_to_add):
+    #         print("done")
+    #
+    # end1 = timer()
+
+    # Non-threading example
+    for _ in users_to_add:
+        create_user(_)
+
+    end2 = timer()
+
+    # print("start: %s" % start)
+    # print("end1: %s" % end1)
+    # print("end2: %s" % end2)
 
 
-# def thread_runner(client, tasks):
+
+
